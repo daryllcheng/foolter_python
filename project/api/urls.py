758e734f398221189c02e5de0eaaed1a033c6e3e
Daryll <daryllcsh@gmail.com>
2018-01-02 15:24:41 -0800

    Return filtered foods

--- project/api/urls.py
+++ project/api/urls.py
@@ -2,9 +2,18 @@ from django.conf.urls import url, include
 from rest_framework import routers
 from project.api import views
 
-router = routers.DefaultRouter()
-
-router.register(r'users', views.UserViewSet)
 urlpatterns = [
-    url(r'^', include(router.urls)),
+    url(r'^foods/$', views.FoodsView),
 ]
+
+#router = routers.DefaultRouter()
+
+# router.register(r'users', views.UserViewSet)
+# router.register(r'foods', views.FoodsView, 'foods')
+
+# urlpatterns = [
+#     url(r'^foods/$', views.FoodsView),
+#     url(r'^users/$', views.UserViewSet),
+# ]
+
+#/(?P<username>\w{0,50})/$
--- project/api/views.py
+++ project/api/views.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
 from project.api.serializers import UserSerializer
+from django.http import JsonResponse
+import json
+import os
 
 class UserViewSet(viewsets.ModelViewSet):
     """
@@ -8,3 +11,23 @@ class UserViewSet(viewsets.ModelViewSet):
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+def FoodsView(request):
+    """
+    API endpoint that retunrs filtered foods
+    """
+    with open(os.path.join(os.path.dirname(__file__),"food_data.json"), 'r') as json_file:
+        data = json.load(json_file)
+        foods = data['report']['foods']
+        params = request.GET.get('query')
+
+        def filter_foods(nutrients, ranges):
+            return is_in_range(nutrients[1], ranges["protein"]) and is_in_range(nutrients[2], ranges["fat"]) and is_in_range(nutrients[3], ranges["carbs"]) and is_in_range(nutrients[4], ranges["sugar"]) 
+        
+        def is_in_range(nutrient, range):
+            gm = nutrient["gm"] if nutrient["gm"] != "--" else 0
+            return range[0] <= gm <= range[1]
+
+        response_data = [food for food in foods if filter_foods(food["nutrients"], params)] if params != None else foods
+        return JsonResponse(data)
